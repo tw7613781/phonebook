b5e5b8a5f659778e07eda20abb2055bf7ab9fc48
Max Tang <tw7613781@gmail.com>
2020-06-22 00:57:37 +0900

    feat: add frontend code and static endpoint

---
+++ build/precache-manifest.4e7560203aae19debb5fd774d2b292f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1276a6990b5d049c870eb8a80307ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d2366ad12728c5d97d",
+    "url": "/static/css/main.455131bb.chunk.css"
+  },
+  {
+    "revision": "b14df7992e95a1a6bafb",
+    "url": "/static/js/2.d8d29dd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8d29dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d2366ad12728c5d97d",
+    "url": "/static/js/main.8604affa.chunk.js"
+  },
+  {
+    "revision": "6be06fbc24171e2fd061",
+    "url": "/static/js/runtime-main.110b1f99.js"
+  }
+]);
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('body', (req) => {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 const PORT = process.env.PORT || 3001
 
