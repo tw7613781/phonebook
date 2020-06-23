ec9632db80ab46a329d3789f44a2ed263c769b63
Max Tang <tw7613781@gmail.com>
2020-06-23 12:49:01 +0900

    feat: add mongodb script to initl data

--- mongo.js
+++ mongo.js
@@ -1,49 +1,60 @@
 const mongoose = require('mongoose')
 
-console.log(process.argv.length)
-
-if (process.argv.length !== 3 && process.argv.length !== 5) {
-    console.log('cli format is incorrect, please follow anyone of blew')
-    console.log('insert => node mongo.js <password> <name> <number>')
-    console.log('select all => node mongo.js <password>')
+if ( !(process.argv.length === 3 || process.argv.length === 5)) {
+    console.log(`
+        cli format is incorrect, please follow blew:
+          add person ===> node mongo.js <password> <name> <number>
+        show persons ===> node mongo.js <passwrod>
+    `)
     process.exit(1)
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
+async function main() {
+    const password = process.argv[2]
+    const name = process.argv[3]
+    const number = process.argv[4]
 
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0-0gg7u.gcp.mongodb.net/person-app?retryWrites=true&w=majority`
+    const url =
+        `mongodb+srv://fullstack:${password}@cluster0-0gg7u.gcp.mongodb.net/person-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    try {
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('connected successful')
+    } catch (e) {
+        throw(e)
+    }
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-})
 
-const Person = mongoose.model('Person', personSchema)
-
-if (!name && !number) {
-    Person.find({}).then((result) => {
-        console.log('phonebook')
-        result.forEach((person) => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String
     })
-} else {
-    const person = new Person({
-        name,
-        number
+
+    const Person = mongoose.model('Person', personSchema, 'persons')
+
+    if (!(name && number)) {
+        Person.find({}).then((result) => {
+            console.log('phonebook')
+            result.forEach((person) => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
         })
-      
-    person.save().then(result => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+    } else {
+        const person = new Person({
+            name,
+            number
+            })
+        
+        person.save().then(result => {
+            console.log(`added ${name} number ${number} to phonebook`)
+            mongoose.connection.close()
+        })
+    }
 }
 
+main().catch( (e)=> {
+    console.log(e)
+})
 
 
